80b42c9a17a7182078af9e9d652c69fc2fc70afb
maxjg5019 <maxjg5019@gmail.com>
2023-03-21 12:21:58 +0800

    移除不必要的Code

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
-const axios = require("axios");
-const { error } = require("console");
 require("dotenv").config();
-// var test = process.env.OPENAI_API_KEY
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
